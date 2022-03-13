083f7c081b5d5399d55821a24db248bbae5d9848
chakravarthy <m.chakra473@gmail.com>
2022-03-13 11:51:25 +0530

    added fibonacci series logic to the uc1 branch

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace logical_problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //fibocci series logic for n numbers;
+            Console.WriteLine("enter the number upto which fibonacci series to be printed");
+            int n=int.Parse(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3 = 0;
+            Console.Write("the fibonacci series of "+n+" are: " +n1 +" "+ n2 +" ");
+            for(int i = 2; i < n; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2= n3;
+            }
+        }
+    }
+}
