27a093cf19d82ecec2f6482a23a63901f613d6b3
chakravarthy <m.chakra473@gmail.com>
2022-03-13 14:10:16 +0530

    added stopwatch logic to the uc4 branch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace logical_problems
 {
@@ -6,34 +7,24 @@ namespace logical_problems
     {
         static void Main(string[] args)
         {
-            //coupon number logic ;
-            Random random = new Random();
-            Console.WriteLine("enter the number of coupon numbers to be generated");
-            int n=int.Parse(Console.ReadLine());
-            Console.WriteLine("the {0} unique coupon numbers are :",n);
-            System.Collections.Generic.List<int> list = new System.Collections.Generic.List<int>();//created list to store random values;
-            int i = 0;
-            while (i < n)
-            {
-                int v = random.Next(10, 20);
-                if (!list.Contains(v))//only enter the condition if list does not contains values of random number i.e.only new number enters to the list;
-                {
-                    list.Add(v);//add the random number to the list
-                    Console.WriteLine(v);
-                    i++;
-
-                }
-                else
-                {
-                    i--;
-                
-               }
-
-            }
-
-          
-           
+            //stopwatch logic ;
             
+            var timer = new Stopwatch ();
+            Console.WriteLine("start the stopwatch by pressing any key");
+            timer.Start();
+           Console.ReadKey();
+            Console.WriteLine("end the stopwatch by pressing any key");
+            Console.ReadKey();
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+            string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+            Console.WriteLine (foo);
+
+
+
+
+
         }
             
         
